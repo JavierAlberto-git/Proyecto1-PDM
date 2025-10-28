7549c01dba733ab479fbd69eca3cbdf57fb5da39
Edric Yahir <aguilarhernandezedricyahir@gmail.com>
2025-10-27 20:35:35 -0600

    Version Final funcionado

--- Practica3Room/app/src/main/java/com/example/practica3room/ui/screens/Navigator.kt
+++ Practica3Room/app/src/main/java/com/example/practica3room/ui/screens/Navigator.kt
@@ -13,5 +13,8 @@ fun Navigator(viewModel: TaskViewModel) {
         composable("menu") { MenuScreen(navController) }
         composable("task_list") { TaskListScreen(navController, viewModel) }
         composable("add_task") { AddTaskScreen(navController, viewModel) }
+        composable("manage_status") { StatusScreen(navController, viewModel) }
+        composable("edit_task") { EditScreen(navController, viewModel) }
+        composable("delete_task") { DeleteTasksScreen(navController, viewModel) }
     }
 }
